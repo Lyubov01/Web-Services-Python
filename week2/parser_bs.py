89cf6dab213e98320836b40bb8cb5ddff5f657d1
UbHTKcQh <lyyubava@gmail.com>
2021-04-06 01:31:32 +0300

    added simple queue for future bfs and implementation of graph as a dict

--- week2/parser_bs.py
+++ week2/parser_bs.py
@@ -1,6 +1,32 @@
 from bs4 import BeautifulSoup
 import unittest
 
+"""
+PART 1
+
+В этом задании вам необходимо реализовать парсер для сбора статистики со страниц Википедии. Чтобы упростить вашу 
+задачу, необходимые страницы уже скачаны и сохранены на файловой системе в директории wiki/ (Например, 
+страница https://en.wikipedia.org/wiki/Stone_Age сохранена файле wiki/Stone_Age).Парсер реализован в виде функции 
+parse, которая принимает на вход один параметр: path_to_file — путь до файла, содержащий html код страницы википедии. 
+Гарантируется, что такой путь существует. Ваша задача — прочитать файл, пройтись Beautiful Soup по статье, 
+найти её тело (это <div id="bodyContent">) и внутри него подсчитать ... 
+
+PART 2 
+
+В этом задании продолжаем работать со страницами из wikipedia. Необходимо реализовать механизм сбора статистики по 
+нескольким страницам. Сложность задачи состоит в том, что сначала нужно будет определить страницы, с которых 
+необходимо собирать статистику. В качестве входных данных служат названия двух статей(страниц). Гарантируется, 
+что файлы обеих статей есть в папке wiki и из первой статьи можно попасть в последнюю, переходя по ссылкам только на 
+те статьи, копии которых есть в папке wiki. 
+
+Например, на вход подаются страницы: Stone_Age и Python_(
+programming_language). В статье Stone_Age есть ссылка на Brain, в ней на Artificial_intelligence, а в ней на Python_(
+programming_language) и это кратчайший путь от Stone_Age до Python_(programming_language). Ваша задача — найти самый 
+короткий путь (гарантируется, что существует только один путь минимальной длины), а затем с помощью функции parse из 
+предыдущего задания собрать статистику по всем статьям в найденном пути. """
+import os
+import re
+
 
 class Parser:
     def __init__(self, path_to_file):
@@ -70,6 +96,81 @@ class Parser:
         return counter
 
 
+def create_tree(path):
+    """построение дерева в виде словаря имя файла: список файлов, на которые он содержит ссылки"""
+    link_re = re.compile(r"(?<=/wiki/)[\w()]+")  # поиск ссылок
+    files = dict.fromkeys(os.listdir(path))  # словарь, который нужно заполнить :)
+
+    for file in files:
+        files[file] = []
+        with open(os.path.join(path, file), encoding="utf-8") as reader:
+            refs_list = link_re.findall(reader.read())
+
+        for ref in set(refs_list):
+            if ref not in files:
+                continue
+            files[file].append(ref)
+    return files
+
+    # files = dict.fromkeys(os.listdir(path))
+    # for file in files:
+    #    files[file] = []
+    #    with open(os.path.join(path, file), 'r', encoding='utf-8') as f:
+    #        refs_list = re.findall(r"(?<=/wiki/)[\w()]+", f.read())
+    #    for ref in set(refs_list):
+    #        try:
+    #            open(os.path.join(path, ref), 'r')
+    #            files[file].append(ref)
+    #        except FileNotFoundError:
+    #            continue
+    #
+
+    # d = {}
+    # first_file = os.path.join(path, start_page)
+    # last_file = os.path.join(path, end_page)
+    # with open(first_file, 'r') as first:
+    #    links = set(re.findall(r"(?<=/wiki/)[\w()]+", first.read()))
+    # for link in links:
+    #    try:
+    #        with open(os.path.join(path, link), 'r') as link_file:
+    #            d[link] = set(re.findall(r"(?<=/wiki/)[\w()]+", link_file.read()))
+    #    except FileNotFoundError:
+    #        continue
+
+
+#
+# return links
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.items:
+            deq_item = self.items[0]
+            self.items.pop(0)
+            return deq_item
+
+
+
+
+
+def get_statistics(path, start_page, end_page):
+    """собирает статистику со страниц, возвращает словарь, где ключ - название страницы,
+    значение - список со статистикой страницы"""
+
+    # получаем список страниц, с которых необходимо собрать статистику
+    pages = build_bridge(path, start_page, end_page)
+    # напишите вашу реализацию логики по сбору статистики здесь
+
+    return statistic
+
+
 def parse(path_to_file):
     # Поместите ваш код здесь.
     # ВАЖНО!!!
@@ -98,4 +199,9 @@ class TestParse(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+    result = build_bridge('wiki/', 'The_New_York_Times', 'Stone_Age')
+    print(result)
+
+    # print('Stone_Age' in os.listdir('wiki'))
+    # print(create_tree('wiki', None, None))
